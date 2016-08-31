2274d6d2d26c0c2686d2129de3e65e6e4921ceab
Adam Keller <akeller@cloudpassage.com>
2016-08-30 18:00:01 -0700

    Updating args and adding requirements.txt for pip deps

--- args.py
+++ args.py
@@ -6,8 +6,8 @@ class GetArgs(object):
         parser = ArgumentParser()
         parser.add_argument('-t', '--token', action='store', dest='token', help='Token', type=str, required=True)
         parser.add_argument('-k1', '--key1', action='store', dest='key1', help='First Unseal Key', type=str, required=True)
-        parser.add_argument('-k2', '--key2', action='store', dest='key2', help='First Unseal Key', type=str, required=True)
-        parser.add_argument('-k3', '--key3', action='store', dest='key3', help='First Unseal Key', type=str, required=True)
+        parser.add_argument('-k2', '--key2', action='store', dest='key2', help='Second Unseal Key', type=str, required=True)
+        parser.add_argument('-k3', '--key3', action='store', dest='key3', help='Third Unseal Key', type=str, required=True)
         return parser.parse_args()
 
 if __name__ == '__main__':
