1dffb3538331ee4c9734ac89cb92e04fa5fd84d9
Adam Keller <akeller@cloudpassage.com>
2016-08-31 15:45:27 -0700

    making tag a parameter that is passed to the class object

--- vaults.py
+++ vaults.py
@@ -4,12 +4,13 @@ from boto3 import client as botoclient
 
 class GetVaults(object):
 
-    def __init__(self, region='us-west-2'):
+    def __init__(self, region='us-west-2', tag='ecs-services'):
         self.region = region
+        self.tag = tag
         self.client = botoclient('ec2', region_name=self.region)
 
     def return_ecs_vaults(self):
-        vaults = [x['Instances'][0]['PrivateIpAddress'] for x in self.client.describe_instances(Filters=[{'Name':'tag:Name', 'Values':['ecs-services']}])['Reservations']]
+        vaults = [x['Instances'][0]['PrivateIpAddress'] for x in self.client.describe_instances(Filters=[{'Name':'tag:Name', 'Values':[self.tag]}])['Reservations']]
         return vaults
 
 
